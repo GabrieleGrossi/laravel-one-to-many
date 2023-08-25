e6305290b4f4082a45186880d31e1d29e6b24d7d
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-25 16:12:22 +0200

    Push for correction. Db undefined Type variable

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
+        $types=[
+            'LongTerm','ShortTerm','Expensive','Cheap'
+        ];
+
+        foreach ($types as $type){
+            $newType = new $Type();
+            $newType->name = $type;
+            $newType->slug = $faker->slug();
+            $newType->save();
+        }
     }
 }
